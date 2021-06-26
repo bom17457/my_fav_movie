7393f49af31cdafb7926b3c2d4f251b888e568e6
Akaporn Katip <akaporn@riverpark.co.th>
2021-06-27 06:54:29 +0700

    config routing

--- projects/my-movies/src/app/app.module.ts
+++ projects/my-movies/src/app/app.module.ts
@@ -3,18 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatCardModule } from '@angular/material/card'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MovieGridComponent } from './movie-grid/movie-grid.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BrowserComponent } from './browser/browser.component';
+import { FavComponent } from './fav/fav.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieGridComponent,
     ToolbarComponent,
+    BrowserComponent,
+    FavComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +30,9 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
