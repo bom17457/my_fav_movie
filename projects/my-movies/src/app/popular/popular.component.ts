75326aa77d1480722b1b2c056e961ff925b8339f
Akaporn Katip <akaporn@riverpark.co.th>
2021-06-29 02:17:24 +0700

    fetch get popular

--- projects/my-movies/src/app/popular/popular.component.ts
+++ projects/my-movies/src/app/popular/popular.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../moviedb/movie.service';
+import { popular } from '../moviedb/movies';
 
 @Component({
   selector: 'app-popular',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popular.component.sass']
 })
 export class PopularComponent implements OnInit {
+  popular?: popular;
 
-  constructor() { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    this.getPopular();
+  }
+
+  getPopular(): void {
+    this.movieService.getPopular().subscribe(popular => {
+      (this.popular = popular)
+      console.log(popular)
+    }, error => {
+
+    });
   }
 
 }
